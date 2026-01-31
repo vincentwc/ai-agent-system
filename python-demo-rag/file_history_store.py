e7ce97dbc913d4d3b540a60a356b8c0b885c818d
git <wang_cheng@cestc.cn>
2026-01-31 12:12:08 +0800

    初步完成历史记录的存储

---
+++ python-demo-rag/file_history_store.py
@@ -0,0 +1,115 @@
+import os
+import json
+from typing import Sequence
+from langchain_core.messages import message_to_dict, messages_from_dict, BaseMessage
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_models.tongyi import ChatTongyi
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+)
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+# message_to_dict 单个消息对象(BaseMessage类实例) -> dict
+# messages_from_dict [字典、字典] -》 [消息、消息]
+
+
+def get_history(session_id: str):
+    return FileChatMessageHistory(session_id, "./chat_history")
+
+
+class FileChatMessageHistory(BaseChatMessageHistory):
+    def __init__(self, session_id: str, storage_path: str):
+        self.session_id = session_id  # 会话id
+        self.storage_path = storage_path  # 不同会话id的存储文件，所在的文件夹路径
+        # 完整的文件路径
+        self.file_path = os.path.join(self.storage_path, self.session_id)
+
+        # 确保文件夹是存在的
+        os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
+
+    def add_messages(self, messages: Sequence[BaseMessage]) -> None:
+        # Sequence序列 类似list、tuple
+        all_messages = list(self.messages)  # 已有的消息列表
+        all_messages.extend(messages)  # 新的和已有的融合成一个list
+
+        # new_messages = []
+        # for message in all_messages:
+        #     d = message_to_dict(message)
+        #     new_messages.append(d)
+        # 简写模式
+        new_messages = [message_to_dict(message) for message in all_messages]
+
+        with open(self.file_path, "w", encoding="utf-8") as f:
+            json.dump(new_messages, f)
+
+    @property  # @property装饰器将messages方法变成成员属性用
+    def messages(self) -> list[BaseMessage]:
+        # 当前文件内。list[字典]
+        try:
+            with open(self.file_path, "r", encoding="utf-8") as f:
+                messages_data = json.load(f)
+                return messages_from_dict(messages_data)
+        except FileNotFoundError:
+            return []
+
+    def clear(self) -> None:
+        with open(self.file_path, "w", encoding="utf-8") as f:
+            json.dump([], f)
+
+
+model = ChatTongyi(model="qwen3-max")
+
+# prompt = PromptTemplate.from_template(
+#     "你需要根据会话历史回应用户问题。对话历史：{chat_history},用户提问:{input}, 请回答"
+# )
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "你需要根据会话历史回应用户问题。"),
+        MessagesPlaceholder("chat_history"),
+        ("human", "请回答如下问题：{input}"),
+    ]
+)
+
+str_parser = StrOutputParser()
+
+
+def print_prompt(full_prompt):
+    # print(">>> 进入 print_prompt，类型为:", type(full_prompt))
+    if hasattr(full_prompt, "to_string"):
+        print("模型输入：", full_prompt.to_string())
+    else:
+        print("模型输入（原始）：", full_prompt)
+    return full_prompt
+
+
+base_chain = prompt | print_prompt | model | str_parser
+
+
+# 实现通过会话id获取InMemoryChatMessageHistory类对象
+def get_history(session_id: str):
+    return FileChatMessageHistory(session_id, "./chat_history")
+
+
+# 创建一个新的链，对原有链增强功能，自动附加历史消息
+conversation_chain = RunnableWithMessageHistory(
+    base_chain,  # 被增强对原有链
+    get_history,  # 通过绘会话id获取InMemoryChatMessageHistory类对象
+    input_messages_key="input",  # 表示用户输入在模版中的占位符
+    history_messages_key="chat_history",  # 表示用户输入在模版中的占位符
+)
+
+
+if __name__ == "__main__":
+    # 固定格式，添加langchain的配置，为当前程序配置所属的session_id
+    session_config = {"configurable": {"session_id": "user_001"}}
+    # res = conversation_chain.invoke({"input": "小明有两个猫"}, session_config)
+    # print("第一次执行" + res)
+
+    # res = conversation_chain.invoke({"input": "小刚有两只猫"}, session_config)
+    # print("第二次执行" + res)
+
+    res = conversation_chain.invoke({"input": "总共有几只宠物"}, session_config)
+    print("第三次执行" + res)
--- python-demo-rag/rag.py
+++ python-demo-rag/rag.py
@@ -2,15 +2,22 @@ from vector_stores import VectorStoreService
 from langchain_community.embeddings import DashScopeEmbeddings
 import config_data as config
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import MessagesPlaceholder
 from langchain_community.chat_models import ChatTongyi
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import (
+    RunnableLambda,
+    RunnablePassthrough,
+    RunnableWithMessageHistory,
+)
 from langchain_core.documents import Document
 from langchain_core.output_parsers import StrOutputParser
+from file_history_store import get_history
+
 
 def print_prompt(prompt):
-    print("="*20)
+    print("=" * 20)
     print(prompt.to_string())
-    print("="*20)
+    print("=" * 20)
     return prompt
 
 
@@ -25,6 +32,8 @@ class RagService(object):
                     "system",
                     "以我提供的已知参考资料为主,简介和专业的回答用户问题。参考资料:{context}.",
                 ),
+                ("system", "并且我提供用户的对话历史记录如下:"),
+                MessagesPlaceholder(variable_name="history"),
                 ("human", "请回答用户问题:{input}"),
             ]
         )
@@ -46,16 +55,48 @@ class RagService(object):
                 format_str += f"文档片段:{doc.page_content}\n:文档元数据:{doc.metadata}"
             return format_str
 
-        chain = {
-            "input": RunnablePassthrough(),
-            "context": retrievers | format_document,
-        } | self.prompt_template | print_prompt | self.chat_model  | StrOutputParser()
-        return chain
+        def format_for_retriever(value):
+            return value["input"]
+
+        def format_for_template(value):
+            # print("----", value)
+            # {input, context, history}
+            new_value = {
+                "input": value["input"]["input"],
+                "context": value["context"],
+                "history": value["input"]["history"],
+            }
+            return new_value
+
+        chain = (
+            {
+                "input": RunnablePassthrough(),
+                "context": RunnableLambda(format_for_retriever)
+                | retrievers
+                | format_document,
+            }
+            | RunnableLambda(format_for_template)
+            | self.prompt_template
+            | print_prompt
+            | self.chat_model
+            | StrOutputParser()
+        )
+
+        conversation_chain = RunnableWithMessageHistory(  # 历史记忆的链
+            chain,
+            get_history,
+            input_messages_key="input",
+            history_messages_key="history",
+        )
+
+        return conversation_chain
 
 
 if __name__ == "__main__":
+    # session_id配置
+    session_config = {"configurable": {"session_id": "user_001"}}
     rag_service = RagService(
         embedding=DashScopeEmbeddings(model=config.embedding_model)
     )
-    res = rag_service.chain.invoke("我的体重180斤,尺码推荐")
+    res = rag_service.chain.invoke({"input": "羽绒服如何保养？"}, session_config)
     print(res)
